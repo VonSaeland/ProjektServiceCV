bd61bc31b89495bf67686c488a4e41d229fbf52a
Björn saeland <von_saeland@hotmail.se>
2016-07-05 14:27:34 +0200

    Add project files.

---
+++ ProjektServiceCV/IService1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProjektServiceCV
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebGet]
+        List<Item> GetItemsList();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Book/{id}")]
+        Item GetItemById(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "AddBook/{name}")]
+        void AddItem(string name);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "UpdateBook/{id}/{name}")]
+        void UpdateItem(string id, string name);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "DeleteBook/{id}")]
+        void DeleteItem(string id);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ ProjektServiceCV/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProjektServiceCV
+{
+    public class Service1 : IService1
+    {
+        //Ny go Bokdatabas!
+        ItemsDBFileEntities ItemDB = new ItemsDBFileEntities();
+
+        public List<Item> GetItemsList() //slänger in "Item" i en lista som sen returneras
+        {
+            return ItemDB.Item.ToList(); //returnera lista med Items 
+        }
+
+        public Item GetItemById(string id) //hitta ett item som har det id som ges utav användaren
+        {
+            int ItemId = Convert.ToInt32(id); //convertera ItemId till int
+
+            return ItemDB.Item.SingleOrDefault(Item => Item.Id == ItemId); //returnera item där Item.Id är samma som ItemId
+        }
+
+        public void AddItem(string name)
+        { 
+            Item item = new Item { ItemName = name }; // säger att ItemName får värdet som name har
+            ItemDB.Item.Add(item); //lägg till item i databas
+            ItemDB.SaveChanges(); //spara andringar
+        }
+
+        public void UpdateItem(string id, string name) // samma som additem förutom att man kan ändra namnet
+        {
+            int itemId = Convert.ToInt32(id);
+
+            Item item = ItemDB.Item.SingleOrDefault(b => b.Id == itemId);
+            item.ItemName = name; //ändra namn
+            ItemDB.SaveChanges();
+
+        }
+
+        public void DeleteItem(string id)
+        {
+            int itemId = Convert.ToInt32(id);
+
+            Item item = ItemDB.Item.SingleOrDefault(b => b.Id == itemId);
+            ItemDB.Item.Remove(item); //tabort item som håller IDt givet 
+            ItemDB.SaveChanges();
+        }
+    }
+}
